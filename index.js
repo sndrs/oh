27049eb0a4f034e0b8a0b8d4d286b471045e75ce
sndrs <a@snd.rs>
2017-03-06 17:50:01 +0000

    remove old stuff

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const handleError = require('./lib/handle-error');
 process.on('unhandledRejection', handleError);
 process.on('uncaughtException', handleError);
 
+// process user input
 const argv = minimist(process.argv.slice(2), {
     boolean: ['version', 'help'],
     alias: { help: 'h', version: 'v' },
@@ -39,7 +40,7 @@ const userTasks = require(ohFile);
 // if we have the help flag or no tasks, show help
 if (argv.help || !argv._.length) {
     showHelp({ userTasks, ohFile });
-    process.exit(0);
+    process.exit(argv.help ? 0 : 1);
 }
 
 // if we have an unkown task/s, show help
@@ -62,15 +63,12 @@ process.chdir(path.dirname(path.resolve(ohFile)));
 
 // get the bits we needs from the args
 const tasksToRun = argv._;
-const userArgs = Object.keys(argv).reduce(
-    (result, arg) => {
-        if (['_', 'h', 'help', 'v', 'version', '$0'].some(_ => _ === arg)) {
-            return result;
-        }
-        return Object.assign(result, { [arg]: argv[arg] });
-    },
-    {}
-);
+const userArgs = Object.keys(argv).reduce((result, arg) => {
+    if (['_', 'h', 'help', 'v', 'version', '$0'].some(_ => _ === arg)) {
+        return result;
+    }
+    return Object.assign(result, { [arg]: argv[arg] });
+}, {});
 
 // turn the functions exported by oh.js into OhTasks
 const runTask = require('./lib/runTask');
